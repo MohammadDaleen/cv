ea88eaa533b253320d91d6472b663f6d071230de
Mohammad Daleen <mohammaddalien2003@gmail.com>
2023-09-03 13:33:49 +0300

    add bio.pythonanywhere.com to allowed hosts

--- cv/settings.py
+++ cv/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c0eh350p3x%m^4eqmb+2yhx*nqb-$ih#!(4zmiu$^u7hz!iuz=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["bio.pythonanywhere.com"]
 
 
 # Application definition
